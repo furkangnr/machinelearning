580e3de23e2035c48b1d234ebb6e956d07e1c651
furkanguner <43773457+furkangnr@users.noreply.github.com>
2019-01-06 14:54:48 +0300

    Simple NN implementation in Keras.

---
+++ Keras_NeuralNets.py
@@ -0,0 +1,56 @@
+'''
+Trains a simple Deep Neural Network on the MNIST dataset.
+'''
+
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import RMSprop
+
+batch_size = 128
+num_classes = 10
+epochs = 20
+
+# the data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train.reshape(60000, 784)
+x_test = x_test.reshape(10000, 784)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+# model building from scratch, for the first layer we must specify input shape. For the last layer we must use 
+# softmax activaion function, I used Dropout layers to avoid overfitting.
+model = Sequential()
+model.add(Dense(512, activation='relu', input_shape=(784,)))
+model.add(Dropout(0.2))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(num_classes, activation='softmax'))
+
+# check model summary
+model.summary() 
+# compile the model before starting training. 
+
+model.compile(loss='categorical_crossentropy',
+              optimizer= RMSprop(),
+              metrics=['accuracy'])
+# training phase 
+history = model.fit(x_train, y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_data=(x_test, y_test))
+# evaluate models performance .
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
